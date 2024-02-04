dbe5d6c2c3abb39c67b1a03e99f278295755e717
SimonDelord <simon.delord@gmail.com>
2024-02-05 10:20:57 +1100

    Update webhook-kafka.py

--- webhook-and-producer/webhook-kafka.py
+++ webhook-and-producer/webhook-kafka.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
 from kafka import KafkaProducer
 import json
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-#@app.route('/webhook', methods=['POST'])
-@app.route('/', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
+#@app.route('/', methods=['POST'])
 
 def hello(): #hello() is registered to route /
     #Gets the POST body as a JSON object
@@ -15,18 +22,14 @@ def hello(): #hello() is registered to route /
     #Initializes the producer and identifies the docker server.
     #kafka-spotify is listed in /etc/hosts with the ip of the container
     #Sets the producer serializer to JSON
-    producer = KafkaProducer(bootstrap_servers='172.30.2.176:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=os.environ["BOOTSTRAP_SERVER"],value_serializer=lambda v: json.dumps(v).encode('utf-8'))
     #Send a message to the kafka topic 'test'
     #passes the POST JSON body as the message
-    producer.send('acs-topic-3', body)
+    producer.send(os.environ["KAFKA_TOPIC"], body)
     #Closes the TCP stream to Kafka
     producer.close()
     #Returns a Complete string
     return "Complete"
-    
-#producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-#producer = KafkaProducer(bootstrap_servers='172.30.2.176:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-#producer = KafkaProducer(bootstrap_servers='172.30.2.176:9092',value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
 if __name__ == '__main__':
 #    app.run(debug=True)
