bd41a92842d0708ebb82261074169dffd82008bf
SimonDelord <simon.delord@gmail.com>
2024-02-05 14:27:28 +1100

    Update kafka-consumer-acs.py

--- kafka-consumer/kafka-consumer-acs.py
+++ kafka-consumer/kafka-consumer-acs.py
@@ -9,6 +9,7 @@ load_dotenv(verbose=True)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def get_acs_parameters():
 
 def main():
   print("Starting consumer", os.environ["BOOTSTRAP_SERVER"])
@@ -23,17 +24,19 @@ def main():
   consumer.subscribe([os.environ["KAFKA_TOPIC"]])
 
   for message in consumer:
- #   print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
- #                                         message.offset, message.key,
- #                                         message.value))
+
     try:
-      kafka_message = f"""
-      Values for AAP - cluster: {message.value['alert']['clusterName']}
-      Values for AAP - namespace: {message.value['alert']['namespace']}
-      Values for AAP - deployment: {message.value['alert']['deployment']['name']}
-
-
-      """
+#      kafka_message = f"""
+#      Values for AAP - cluster: {message.value['alert']['clusterName']}
+#      Values for AAP - namespace: {message.value['alert']['namespace']}
+#      Values for AAP - deployment: {message.value['alert']['deployment']['name']}
+#      """
+      kafka_message = {
+      "Values for AAP - cluster": {message.value['alert']['clusterName']}
+      "Values for AAP - namespace": {message.value['alert']['namespace']}
+      "Values for AAP - deployment": {message.value['alert']['deployment']['name']}
+      }
+      
       logger.info(kafka_message)
     except Exception as e:
       logger.error(e)
